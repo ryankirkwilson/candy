50357e66e27bf9c5f3d6bb1ad54522ecb133bab5
Ryan Wilson <ryanw@rollingpaperdepot.com>
2020-02-10 18:08:04 -0700

    ghost added
    ghost added

--- candy/src/templates/index.js
+++ candy/src/templates/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
